b3155656625437019571dc32099b375e48712437
mcbridz@gmail.com <mcbridz@gmail.com>
2021-02-24 11:11:49 -0800

    Initial model setup

--- myailife_proj/myailife_app/models.py
+++ myailife_proj/myailife_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    text = models.TextField(blank=True)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    post = models.ForeignKey(
+        Post, on_delete=models.PROTECT, related_name='comments')
+    is_reply = models.BooleanField()
+    reply = models.ForeignKey(
+        "self", on_delete=models.PROTECT, related_name='replies', blank=True, null=True)
